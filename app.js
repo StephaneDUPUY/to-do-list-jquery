470621ece6ea0f1bd562d22736e641a5b7a074ad
Stéphane DUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-11-20 15:26:07 +0100

    delete task function added

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ $(document).ready(function () { // wait end of load to apply script
         if (value !== '') { // check if value string is not empty
             console.log('champ rempli');
             var el = $("<li></li>").text(value); // create li element with value filled and store it in el
-            $(el).append("<button class='delete'>X</button>"); // add button X element to li element (el)
+            $(el).append(" <button class='delete'>X</button>"); // add button X element with class 'delete' to li element (el)
             $("#list").append(el); // on #list target, add el element created
-            $("input").val(''); // empty the input field
+            $("input").val(''); // empty the input field with target input
+            $(".delete").on('click', function () { // target on .delete class and listen on click event
+                $(this).parent().remove(); // remove parent of X button (remove li element), "this" is the current element targetted (button)
+            });
         } else {
-            alert('Merci d\'indiquer le nom de la tâche à ajouter'); // popup message on filed empty when list added
-            console.log('champ non rempli')
+            alert('Merci d\'indiquer le nom de la tâche à ajouter à votre liste'); // popup message on filed empty when list added
+            console.log('champ non rempli');
         }
     });
 });
