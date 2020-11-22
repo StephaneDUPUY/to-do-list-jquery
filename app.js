b14b7ae89a8e4ef56fba917b4a0ccfbb3ce50303
StephaneDUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-11-22 02:09:14 +0100

    bootstrap style finished

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ $(document).ready(function () { // wait end of load to apply script
         let value = $("input").val(); // retrieve value filled on input
         if (value !== '') { // check if value string is not empty
             console.log('champ rempli');
-            var el = $("<li></li>").text(value); // create li element with value filled and store it in el
-            $(el).append(" <button class='delete'>X</button>"); // add button X element with class 'delete' to li element (el)
+            var el = $("<li class='list-group-item list-group-item-primary rounded-lg mt-2 mx-auto py-0'></li>").text(value); // create li element with value filled and store it in el
+            $(el).append(" <button class='delete btn btn-danger btn-sm my-1 ml-5'>X</button>"); // add button X element with class 'delete' to li element (el)
             $("#list").append(el); // on #list target, add el element created
             $("input").val(''); // empty the input field with target input
             $(".delete").on('click', function () { // target on .delete class and listen on click event
